176343750315a41f69a0b13edd43be2337c32bd9
Sophie Minger <43566163+minsopV2@users.noreply.github.com>
2019-10-04 15:20:49 +0200

    Choix
    Base de menu faite on peut choisir la difficulté

--- TheHangmanGames/src/thehangmangames/TheHangmanGames.java
+++ TheHangmanGames/src/thehangmangames/TheHangmanGames.java
@@ -6,11 +6,17 @@
 package thehangmangames;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.Labeled;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 /**
@@ -19,33 +25,51 @@ import javafx.stage.Stage;
  */
 public class TheHangmanGames extends Application {
     
-    @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    /**
-     * @param args the command line arguments
-     */
+    Label choixDifficulte = new Label("Choix de la difficulté : None");
+      
     public static void main(String[] args) {
         launch(args);
     }
+ 
+    @Override
+    public void start(Stage primaryStage) {    
+                //Creation des boutons
+        ToggleButton choixfacile = new ToggleButton("Facile");
+        ToggleButton choixMoyen = new ToggleButton("Moyen");
+        ToggleButton choixDifficile = new ToggleButton("Difficile");
+        
+        //Ajout dans un group
+        ToggleGroup choixGroupe = new ToggleGroup();
+        choixGroupe.getToggles().addAll(choixfacile, choixMoyen, choixDifficile);
+        
+        //Ajout d'un "écouteur"
+        choixGroupe.selectedToggleProperty().addListener(this::changed);
+        
+        //Création d'un nouveau label
+        Label utilisateurDifficulte = new Label("Difficulté choisie :");
+        
+        HBox boutonChoix = new HBox(choixfacile, choixMoyen, choixDifficile);
+        boutonChoix.setSpacing(10);
+        
+        VBox root = new VBox(choixDifficulte,utilisateurDifficulte,boutonChoix);
+        root.setSpacing(10);
+        root.setStyle("-fx-padding: 10;"+ "-fx-border-style:solid inside;"+"-fx-border-width: 2;"+"-fx-border-insets:5;"+"-fx-border-radius:5;"+"-fx-border-color: blue;");
+        
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Jeu du pendue || Menu");
+        primaryStage.show(); 
     
+    }
+    
+    public void changed(ObservableValue<? extends Toggle> observable, Toggle oldBtn, Toggle newBtn){
+        String selectedLabel = "None";
+        if(newBtn != null){
+            selectedLabel = ((Labeled)newBtn).getText();
+        }
+        choixDifficulte.setText("Choix de la difficulté : "+ selectedLabel);
+    }
+       public void Menu (Stage primaryStage){
+  
+    }
 }
