b1db4c5a3b91b53ef96a1be7c35bcef4370f9ec1
Sophie Minger <43566163+minsopV2@users.noreply.github.com>
2019-10-28 14:52:52 +0100

    Ajout des méthode manquantes
    Suivit du tutoriels trouvée, ajout des dernières méthodes

--- TheHangmanGames/src/thehangmangames/TheHangmanGames.java
+++ TheHangmanGames/src/thehangmangames/TheHangmanGames.java
@@ -5,7 +5,6 @@
  */
 package thehangmangames;
 
-import thehangmangames.LecteurMots.java;
 
 
 import java.util.HashMap;
@@ -76,8 +75,90 @@ public class TheHangmanGames extends Application {
         ligneLettres.setAlignment(Pos.CENTER);
         lettres = ligneLettres.getChildren();
         
+        jouable.bind(hangman.live.greaterThan(0).and(lettreADeviner.greaterThan(0)));
+        jouable.addListener((obs, old, newValue) ->{
+            if(!newValue.booleanValue())
+                arretJeu();
+        });
         
+        Button btnRejouer = new Button("Nouvelle partie !");
+        btnRejouer.disableProperty().bind(jouable);
+        btnRejouer.setOnAction(event -> demarrageJeu());
+        
+        HBox ligne1 = new HBox();
+        HBox ligne3 = new HBox();
+        ligne1.setAlignment(Pos.CENTER);
+        ligne3.setAlignment(Pos.CENTER);
+        for (int i = 0 ; i < 20; i++) {
+            ligne1.getChildren().add(new Lettre(' '));
+            ligne3.getChildren().add(new Lettre(' '));
+        }
+        
+        HBox ligneAlphabet = new HBox(5);
+        ligneAlphabet.setAlignment(Pos.CENTER);
+        
+        for (char c = 'A'; c <= 'Z'; c++) {
+            Text t = new Text(String.valueOf(c));
+            t.setFont(DEFAULT_FONT);
+            alphabet.put(c, t);
+            ligneAlphabet.getChildren().add(t);
+        }
+
+        Text tiret = new Text("-");
+        tiret.setFont(DEFAULT_FONT);
+        alphabet.put('-', tiret);
+        ligneAlphabet.getChildren().add(tiret);
+
+        Text texteScore = new Text();
+        texteScore.textProperty().bind(score.asString().concat(" Points"));
+
+        HBox LignePendu = new HBox(10, btnRejouer, texteScore, hangman);
+        LignePendu.setAlignment(Pos.CENTER);
+
+        VBox vBox = new VBox(10);
+        
+        // vertical layout
+        vBox.getChildren().addAll(
+                ligne1,
+                ligneLettres,
+                ligne3,
+                ligneAlphabet,
+                LignePendu);
+        return vBox;
     }
+    private void arretJeu(){
+        for (Node n : lettres){
+            Lettre lettre = (Lettre) n;
+            lettre.show();
+            
+        }
+    }
+    private void demarrageJeu(){
+        for (Text t : alphabet.values()) {
+            t.setStrikethrough(false);
+            t.setFill(Color.BLACK);
+        }
+        //Tirer le mot aléatoirement 
+        hangman.reset();
+        mot.set(LecteurMots.MotsAleatoires().toUpperCase());
+        lettreADeviner.set(mot.length().get());
+       
+        lettres.clear();
+       for (char c : mot.get().toCharArray()){
+           lettres.add(new Lettre(c));
+       }
+    }
+    private static class HangmanImage extends Parent {
+        private static final int SPINE_START_X = 100;
+        private static final int SPINE_START_Y = 20;
+        private static final int SPINE_END_X = SPINE_START_X;
+        private static final int SPINE_END_Y = SPINE_START_Y + 50;
+        
+
+        
+    
+        
+       
      
      
     
